38b2d637cf7c31937da36b1a2eb73f3b1d84d70a
Henrik Hedström <henrik.hedstrom@hotmail.com>
2024-01-26 23:19:19 +0100

    added debug mode toggle on 'e'

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ window.onload = async () => {
 const config = { backend: 'webgl', modelBasePath: 'models/' };
 const human = new Human.Human(); // create instance of Human
 const outputCanvas = document.getElementById('canvas-id');
+let drawDebug = true;
 
 async function drawResults() {
   const interpolated = human.next(); // get smoothened result using last-known results
   human.draw.canvas(human.webcam.element, outputCanvas); // draw current webcam frame
-  human.draw.all(outputCanvas, interpolated); // draw the frame detectgion results
+  if(drawDebug)
+  {
+    human.draw.all(outputCanvas, interpolated); // draw the frame detection results
+  }
   requestAnimationFrame(drawResults); // run draw loop
 }
+
+document.onkeypress = function (e) {
+  e = e || window.event;
+  drawDebug = !drawDebug
+  console.log("switching debugdraw")
+};
 
 await human.webcam.start({ crop: true });
 outputCanvas.width = human.webcam.width;
