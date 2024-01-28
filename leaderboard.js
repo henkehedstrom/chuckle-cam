7b89fdeae92e32b04523abe1508ebb254132778a
Henrik Hedström <henrik.hedstrom@hotmail.com>
2024-01-28 09:24:39 +0100

    refresh and scrolling

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,36 @@
 let leaderboard;
 
+var scrollingDown = true;
+var intervalId;
+
+var scrollDistance = 1;
+
+var Height = document.documentElement.scrollHeight;
+var currentHeight = 0;
+var bool = true;
+var step = 2;
+var speed = 80;
+var interval = setInterval(scrollpage, speed)
+
+
+    function scrollpage() {
+        if (currentHeight < 0 || currentHeight > Height) 
+            bool = !bool;
+            
+        if (bool) {
+            window.scrollTo(0, currentHeight += step);
+        } else {
+            window.scrollTo(0, currentHeight -= step);
+        }
+        console.log(currentHeight);
+
+    }
+
+var loadinterval = setInterval(() => {
+    location.reload();
+}, (60000));
+
+
 window.onload = async () => {
 	
     leaderboard = document.getElementById("leaderboard");
@@ -33,11 +64,15 @@ function addUser(userInfo,name)
 
 }
 
+
+
+
 function addImage(imageData,tr)
 {
     const image = document.createElement("img");
     const td = document.createElement("td");
     image.src = imageData;
+    image.className = "leaderboardImage";
     td.appendChild(image);
     tr.appendChild(td);
 }
